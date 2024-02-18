4ce61cdb1b60d45d8e347d760b4e3e55ef4178c5
ShaniFroind <150084469+ShaniFroind@users.noreply.github.com>
2024-02-18 17:47:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -91,4 +91,4 @@ def not_found(error):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    app.run(dhost='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
